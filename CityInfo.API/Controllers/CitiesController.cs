209c2ea5addc498b71a79733167dfbd20773c90f
Dorina Dumbrava <drndumbrava@gmail.com>
2018-10-28 08:12:42 +0200

    all

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            var result = _list;
+            return Ok(result);
+        }
+
+
+        static List<object> _list = new List<object>()
+                {
+                new {id = 1, Name = "New York city" },
+                new {id = 2, Name = "Antwerp" }
+                };
+    }
+}
